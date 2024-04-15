b1cee55d558725c3f33d331b470dfddb072ecf21
lecroissant795 <st22424@ormiston.school.nz>
2024-04-15 17:34:30 +1200

    profile picture added

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,9 +1,11 @@
-import { Box, Divider, IconButton, Stack } from "@mui/material"
+import { Avatar, Box, Divider, IconButton, Stack } from "@mui/material"
 import { useTheme } from "@mui/material/styles"
 import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Logo from "../../assets/Images/send-it-logo.png";
 import { Nav_Buttons } from "../../data";
+import { Gear } from "phosphor-react";
+import { faker } from '@faker-js/faker';
 
 const DashboardLayout = () => {
 
@@ -16,14 +18,26 @@ const DashboardLayout = () => {
   return (
     <>
       <Box
-        p={2}
+        borderRadius={5}
+        padding={2}
         sx={{
           backgroundColor: theme.palette.background.paper,
           boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
           height: "100vh",
-          width: "100px"
+          width: "80px"
         }}>
-        <Stack direction="column" alignItems={"center"} sx={{ width: "100%" }} spacing={3}>
+        <Stack 
+          direction="column" 
+          alignItems={"center"} 
+          justifyContent={"space-between"}
+          sx={{ height: "100%" }} 
+          spacing={3}>
+          <Stack 
+           alignItems={"center"}
+           spacing={7}
+           >
+            
+
 
           <Box sx={{
 
@@ -34,14 +48,41 @@ const DashboardLayout = () => {
             <img src={Logo} alt="" />
 
           </Box>
-          <Stack sx={{ width: "max-content" }} direction="column" alignItems="center" spacing={3}>
-            {Nav_Buttons.map((el) => <IconButton key={el.index}>
-              {el.icon}
-            </IconButton>
-          )}
-          <Divider />  
+          <Stack sx={{ width: "max-content" }} direction="column" alignItems="center" spacing={5}>
+            {Nav_Buttons.map((el) => (
+              el.index === selected ?
+              <Box 
+              p={1}
+              sx={{
+                backgroundColor: theme.palette.primary.main, 
+                borderRadius: 1.5}}
+                > 
+                <IconButton 
+                sx={{width:"max-content", color: "white"}} 
+                key={el.index}
+                >
+                  {el.icon}
+                  </IconButton>
+              </Box>
+              : <IconButton 
+              onClick={() => setSelected(el.index)}
+              sx={{width:"max-content", color: "black"}} 
+              key={el.index}
+              >
+                {el.icon}
+                </IconButton>
+
+          ))}
+          <Divider sx={{width:"48px"}}/>  
+          <IconButton>
+            <Gear />
+          </IconButton>
           </Stack>
-          
+          </Stack>
+          <Stack>
+            <Avatar src={faker.image.avatar()}/>
+
+          </Stack>
         </Stack>
       </Box>
       <Outlet />
