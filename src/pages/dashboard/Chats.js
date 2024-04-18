8a4345a256d642d2d27851f835040bde1b0ca801
lecroissant795 <st22424@ormiston.school.nz>
2024-04-18 17:46:11 +1200

    Update Chats.js

--- src/pages/dashboard/Chats.js
+++ src/pages/dashboard/Chats.js
@@ -1,7 +1,75 @@
-import { Box, IconButton, Stack, Typography, InputBase, Button, Divider } from '@mui/material';
+import { Box, IconButton, Stack, Typography, InputBase, Button, Divider, Avatar, Badge } from '@mui/material';
 import React from 'react';
 import { styled, alpha } from '@mui/material/styles';
+import { faker } from '@faker-js/faker';
 import { ArchiveBox, CircleDashed, MagnifyingGlass } from 'phosphor-react';
+
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+        backgroundColor: '#44b700',
+        color: '#44b700',
+        boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+        '&::after': {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            borderRadius: '50%',
+            animation: 'ripple 1.2s infinite ease-in-out',
+            border: '1px solid currentColor',
+            content: '""',
+        },
+    },
+    '@keyframes ripple': {
+        '0%': {
+            transform: 'scale(.8)',
+            opacity: 1,
+        },
+        '100%': {
+            transform: 'scale(2.4)',
+            opacity: 0,
+        },
+    },
+}));
+
+
+const ChatElement = () => {
+    return (
+        <Box
+            sx={{
+                width: "100%",
+                height: 60,
+                borderRadius: 1,
+                backgroundColor: "white",
+            }}
+            p={2}
+        >
+            <Stack direction="row" alignItems="center" justifyContent="space-between">
+                <Stack direction="row" spacing={2}>
+                    <StyledBadge
+                        overlap="circular"
+                        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                        variant="dot">
+
+                        <Avatar src={faker.image.avatar()} />
+                    </StyledBadge>
+                    <Stack spacing={0.3}>
+                        <Typography variant="subtitle2">
+                            Shrey
+                        </Typography>
+                        <Typography variant="caption">
+                            Hi
+                        </Typography>
+                    </Stack>
+
+                </Stack>
+
+            </Stack>
+        </Box>
+    )
+}
 
 
 const Search = styled("div")(({ theme }) => ({
@@ -51,7 +119,7 @@ const Chats = () => {
                     alignItems={"center"}
                     justifyContent={"space-between"}>
                     <Typography variant='h5'>
-                        Chats
+                        Messages
                     </Typography>
                     <IconButton>
                         <CircleDashed size={32} />
@@ -69,16 +137,19 @@ const Chats = () => {
                     </Search>
                 </Stack>
                 <Stack spacing={1}>
-                    <Stack 
-                    direction={"row"} 
-                    alignItems={"center"} 
-                    spacing={1.5}
+                    <Stack
+                        direction={"row"}
+                        alignItems={"center"}
+                        spacing={1.5}
                     >
                         <ArchiveBox size={24} />
                         <Button>Archive</Button>
 
                     </Stack>
                     <Divider />
+                </Stack>
+                <Stack direction="column">
+                    <ChatElement />
                 </Stack>
             </Stack>
         </Box>
